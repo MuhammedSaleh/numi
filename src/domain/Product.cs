afde4b375b4a044c149735243c7785507bafb0bb
Mohamed Saleh <muhammed_saleh@hotmail.com>
2024-11-17 22:54:07 +0200

    initial commit

---
+++ src/domain/Product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+    public string Id { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Dimension { get; set; }
+}
